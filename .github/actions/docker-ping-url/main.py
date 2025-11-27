a39320f4e3f6ce5ceaf8c5834c6286438125a6f2
Ma-Singo <codesingo@gmail.com>
2025-11-27 13:58:29 +0100

    fix(17-3-custom-action): cleaning up code

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,31 @@
+import os
+import requests
+from time import sleep
+
+def ping_url(url, max_trials, delay):
+    trials = 0
+
+    while trials < max_trials:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable")
+            return True
+        except requests.ConnectionError():
+            print(f"Website {url} is unreachable")
+
+        finally:
+            trials += 1
+            sleep(delay)
+
+def run():
+    website_url = os.getenv("INPUT_URL")
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+    delay = int(os.getenv("INPUT_DELAY"))
+
+    ping_url(website_url, max_trials, delay)
+
+
+
 if __name__ == "__main__":
     print("Hello World!")
